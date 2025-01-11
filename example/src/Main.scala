366a5b992b049513cdcb40cd8d6f540c7b19ead7
Aron Bergur Johannsson <aronbergurj@outlook.com>
2025-01-11 21:31:24 +0000

    Configure example app to render with laminar

--- example/src/Main.scala
+++ example/src/Main.scala
@@ -1,7 +1,24 @@
 package example
 
+import com.raquo.laminar.api.L.{*, given}
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation
+
+import org.scalajs.dom
+
+
 object Main {
-    def main(args: Array[String]) = {
+    def main(args: Array[String]): Unit = {
         println("hello world")
+        renderOnDomContentLoaded(
+            dom.document.getElementById("app"),
+            appElement,
+        )
     }
+
+    def appElement = 
+        div(
+            h1("hello world")
+        )
 }
